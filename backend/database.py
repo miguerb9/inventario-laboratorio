b239890dbcf4832dbc73e2c890da227b8be6863b
Miguel <migue.rb9@gmail.com>
2026-07-11 19:49:01 +0200

    No sobreescribir variables de entorno en rpdo

--- backend/database.py
+++ backend/database.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+load_dotenv(override=False)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
