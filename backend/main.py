11cf7adfe8c6cbfca16bff9b5fcfe2a92c96e52b
Miguel <migue.rb9@gmail.com>
2026-07-12 13:22:42 +0200

    peticiones backend

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,10 @@ app = FastAPI(docs_url=None, redoc_url=None) if os.getenv("RENDER") else FastAPI
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://inventario-laboratorio-ten.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
